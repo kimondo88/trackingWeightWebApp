f633a2186d042da427de787285c9fac3e6c460bb
kimondo88 <kimondo1988@o2.pl>
2021-07-07 15:38:36 +0200

    corrected code for labels, need to check month (capped to 30 days)

--- src/utils.js
+++ src/utils.js
@@ -51,25 +51,37 @@ async function checkForTheSameDay(timeToCheck){
 }
 
 async function populateLabel(id){
-    const label = [];
+    const label1 = [];
     const dayOfWeek = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']; 
     const {data} = await axios.get(`http://localhost:3000/users/${id}`); 
     const dataTrackDay = data.trackDay || [];
     let checkForDay = dataTrackDay.pop(); 
     let timeToCheck = Object.keys(checkForDay)[0];
-    const f = new Date(parseInt(timeToCheck)).getDate();
+    const f = new Date(parseInt(timeToCheck)).getDate()
     timeToCheck = new Date(parseInt(timeToCheck))
     let temp = timeToCheck.toString();
     temp = temp.slice(0, 2);
-    label.push(temp);
-    let day = dayOfWeek.indexOf( item => item === temp );  
+    label1.push(temp);
+    let day = dayOfWeek.indexOf( item => item === temp );
+    let dayToday = day;  
     for(let i = 1; i <= f; i++){
         if(day > 0){
             day -= 1; 
+        }else if(day === 0){ 
         }else{ day = 6}
+        label1.push(dayOfWeek[day]); 
+    };
+
+    label1.reverse();
+    const label = Array.from(label1);  
+    day = dayToday;
+
+    for(let i = f; i <= 30; i++){
+        if(day >= 0 && day < 6){  day += 1; }
+        else{ day = 0;}
         label.push(dayOfWeek[day]); 
     }; 
-    return label.reverse();
+    return label;
 };
 
 async function sameMonth(compare1, compare2){
